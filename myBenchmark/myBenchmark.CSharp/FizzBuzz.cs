ad42e30ae2a163587c495003bf2f94ea0a682883
essic <essic@users.noreply.github.com>
2018-07-05 14:24:07 +0200

    First fizz buzz implementation on C# side and adding nuget package

---
+++ myBenchmark/myBenchmark.CSharp/FizzBuzz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace myBenchmark.CSharp
+{
+    public class Range
+    {
+        public Range(int start, int end)
+        {
+            if (start <= 0 || end < 0 && end <= start)
+                throw new ArgumentException($"The interval [start,end] given is incorrect : [{start},{end}]");
+
+            Start = start;
+            End = end;
+        }
+
+        public int Start { get; }
+        public int End { get; }
+
+    }
+
+    public class FizzBuzz
+    {
+        private const string fizz = "Fizz";
+        private const string buzz = "Buzz";
+
+        public string[] NonOptimized(Range range)
+        {
+            var result = new List<string>();
+            for (var number = range.Start; number < range.End; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    result.Add(fizz + buzz);
+                }
+                else if (number % 3 == 0)
+                {
+                    result.Add(fizz);          
+                }
+                else if (number % 5 == 0)
+                {
+                    result.Add(buzz);
+                } else
+                {
+                    result.Add($"{number}");
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
--- myBenchmark/myBenchmark.Runner/Program.cs
+++ myBenchmark/myBenchmark.Runner/Program.cs
@@ -3,13 +3,18 @@ using myBenchmark.CSharp;
 using myBenchmark.FSharp;
 
 namespace myBenchmark.Runner
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
             Say.hello();
-            new Class1().Say();
+
+            var fizzer = new FizzBuzz().NonOptimized(new Range(1, 100));
+            foreach(var f in fizzer )
+            {
+                Console.WriteLine(f);
+            }
             Console.Write("Press any key to exists ...");
             Console.ReadKey();
         }
